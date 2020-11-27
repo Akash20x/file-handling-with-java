583f345cf1ddcd4fcfd25fe774dcda81c201a2e0
Akash20x <46225357+Akash20x@users.noreply.github.com>
2020-11-28 00:41:47 +0530

    Add files via upload

---
+++ uppercase.java
@@ -0,0 +1,33 @@
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.FileWriter;
+
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		
+		try {
+			FileReader reader = new FileReader("write.txt");
+			FileWriter writer = new FileWriter("e.txt");
+
+			int data;
+			int data2;
+			while((data=reader.read())!= -1) {
+			  data2=Character.toUpperCase(data);
+			  writer.write(data2);
+			}
+			reader.close();
+	        writer.close();
+
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+
