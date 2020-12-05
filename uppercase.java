c04f49da1b0adbc8ff75a2e31a91107a15cf32d5
Akash20x <46225357+Akash20x@users.noreply.github.com>
2020-12-06 01:22:59 +0530

    Update uppercase.java

--- uppercase.java
+++ uppercase.java
@@ -4,30 +4,31 @@ import java.io.IOException;
 import java.io.FileWriter;
 
 
-public class uppercase{
+public class uppercase {
 
 	public static void main(String[] args) {
 		
-		try {
-			FileReader reader = new FileReader("write.txt");
-			FileWriter writer = new FileWriter("e.txt");
-
-			int data;
-			int data2;
-			while((data=reader.read())!= -1) {
-			  data2=Character.toUpperCase(data);
-			  writer.write(data2);
-			}
-			reader.close();
-	        writer.close();
-
-			
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+      try {
+        FileReader reader = new FileReader("e.txt");
+        String result = "";
+        int data;
+        int data2;
+        while ((data = reader.read()) != -1) {
+           data2 = Character.toUpperCase(data);
+           result += (char)data2;
+        }
+        reader.close();
+        FileWriter writer = new FileWriter("e.txt");
+        writer.write(result);
+        writer.flush();
+        writer.close();
+         }
+ 
+      catch (FileNotFoundException e) {
+            e.printStackTrace();
+    } catch (IOException e) {
+         e.printStackTrace();
+    }
+  }
 }
-
 
